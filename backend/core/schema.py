acb23a8b6128ef0fa0d36c09c60194d4d544c10a
parthsujalshah <parthsujalshah@gmail.com>
2021-08-11 15:40:20 +0530

    crud list item

--- backend/core/schema.py
+++ backend/core/schema.py
@@ -1,10 +1,11 @@
 import users.schema
+import sample_list.schema
 import graphene
 
-class Mutation(users.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, sample_list.schema.Mutation, graphene.ObjectType):
     pass
 
-class Query(users.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, sample_list.schema.Query, graphene.ObjectType):
     pass
 
 
--- backend/sample_list/models.py
+++ backend/sample_list/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class ListItem(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='list')
     image = models.ImageField(upload_to='list_item_images', blank=True)
 
