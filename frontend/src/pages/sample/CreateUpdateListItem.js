9e202d1abdc72905a097076cfa336dea2b33023f
parthsujalshah <parthsujalshah@gmail.com>
2021-08-12 05:35:45 +0530

    routing

--- frontend/src/pages/sample/CreateUpdateListItem.js
+++ frontend/src/pages/sample/CreateUpdateListItem.js
@@ -5,6 +5,20 @@ const CreateUpdateListItem = props => {
     const [title, setTitle] = React.useState("");
     const [description, setDescription] = React.useState("");
     const [image, setImage] = React.useState("");
+
+    React.useEffect(() => {
+        if (props.match.params.listItemId) {
+            const itemDetails = {
+                title: "t",
+                description: "d",
+                image: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            }
+            setTitle(itemDetails.title);
+            setDescription(itemDetails.description);
+            setImage(itemDetails.image);
+        }
+    }, []);
+
     return (
         <div>
             <form onSubmit={event => {
@@ -15,18 +29,18 @@ const CreateUpdateListItem = props => {
                     Title:
                     <input type="text" value={title} onChange={event => {
                         setTitle(event.target.value);
-                    }}/>
+                    }} />
                 </label>
                 <label>
                     Description:
                     <input type="text" value={description} onChange={event => {
                         setDescription(event.target.value);
-                    }}/>
+                    }} />
                 </label>
                 <label>
                     Image:
                     <img height={20} src={image} />
-                    <input type="file" onChange={event => setImage(event.target.files[0])}/>
+                    <input type="file" onChange={event => setImage(event.target.files[0])} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
