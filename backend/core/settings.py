3dc03d5837079b500965603ba5252af09ad57faf
parthsujalshah <parthsujalshah@gmail.com>
2021-08-17 17:07:36 +0530

    dj postgres

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -86,11 +86,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": 'django.db.backends.postgresql_psycopg2',
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "db",
+            "PORT": 5432,
+        }
     }
-}
 
 
 # Password validation
