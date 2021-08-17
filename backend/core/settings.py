1432fff2cf19137ba968cf9ca8108a97b0e8675c
parthsujalshah <parthsujalshah@gmail.com>
2021-08-17 10:21:28 +0530

    temp

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', default='secret-key-of-at-least-50-characters-to-pass-check-deploy')
+SECRET_KEY = 'django-insecure-yyge^oih6qp-74t(tdgfermyxtakvw6vz-xco5*qakxme@ds@f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,12 +85,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+IN_DOCKER = bool(os.getenv('IN_DOCKER', default=False))
+if IN_DOCKER:
+    DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("SQL_DATABASE", default="startertemplate"),
+        "USER": os.environ.get("SQL_USER", default="startertemplate"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", default="startertemplate"),
+        "HOST": os.environ.get("SQL_HOST", default="db"),
+        "PORT": os.environ.get("SQL_PORT", default="5432"),
     }
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
