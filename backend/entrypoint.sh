4d8a45026efc53fa50de1b0dfb05398a8283351c
parthsujalshah <parthsujalshah@gmail.com>
2021-08-29 12:27:38 +0530

    collectstatic

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+echo "here"
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; \
-    do \
-        sleep 0.1
-    done
+#     while ! nc -z $SQL_HOST $SQL_PORT; \
+#     do \
+#         sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
+#     echo "PostgreSQL started"
+# fi
 
 python manage.py migrate
-
-exec "$@"
+python manage.py collectstatic --no-input
+gunicorn core.wsgi --bind 0.0.0.0:8000
+# exec "$@"
