ff4f001b844fa4c0d024df7b413720db63e61b58
parthsujalshah <parthsujalshah@gmail.com>
2021-08-30 09:10:02 +0530

    added port number to api url

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -177,6 +177,6 @@ GRAPHQL_JWT = {
 # CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
